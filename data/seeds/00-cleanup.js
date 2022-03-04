0f3b5b4528636daf0b90cabff22e2a29fea60609
Yuriko Takamiya <yurikotakamiya@yurikonombp-2.mynetworksettings.com>
2022-03-04 12:15:31 -0500

    clean db and set seed for users table

--- data/seeds/00-cleanup.js
+++ data/seeds/00-cleanup.js
@@ -1,12 +1,7 @@
-const { cleaner } = require('knex-cleaner')
+const { clean } = require('knex-cleaner')
 exports.seed = function(knex) {
-  return cleaner('knex').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return clean(knex, {
+    mode: 'truncate',
+    ignoreTable: ['knex_migrations', 'knex_migrations_lock']
+  })
 };
