c352bd43c5da35f6281eed5673a298b47e6ea865
Yuriko Takamiya <yurikotakamiya@yurikonombp-2.mynetworksettings.com>
2022-03-11 19:10:28 -0500

    seeds data for trades in place

--- data/seeds/05-trades.js
+++ data/seeds/05-trades.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex) {
-  return knex('orders').del()
+  return knex('trades').del()
     .then(function () {
-      return knex('orders').insert([
+      return knex('trades').insert([
         {user_id: 1, strategy_id: 1, trading_pair: 'BTCUSDT', order_size: 1.5, order_price: 39561.90, order_side: 'buy'}
       ])
     })
