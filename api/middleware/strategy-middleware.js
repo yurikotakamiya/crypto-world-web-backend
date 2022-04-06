fbaa85967accdc8552b073fe2118659673c10304
Yuriko Takamiya <yurikotakamiya@yurikonombp-2.mynetworksettings.com>
2022-04-06 13:48:55 -0400

    implemented deleting strategy config endpoint

--- api/middleware/strategy-middleware.js
+++ api/middleware/strategy-middleware.js
@@ -46,27 +46,4 @@ const existsConfig = (req, res, next) => {
         .catch(e => next(e))
 }
 
-const compareChange = (req, res, next) => {    
-    const { exchange_id,
-            api_key,
-            secret_key } = req.api
-    let changes = []
-    if (exchange_id != req.body.exchange_id) {
-        changes["exchange_id"] = req.body.exchange_id
-    } 
-    if (api_key != req.body.api_key) {
-        changes["api_key"] = req.body.api_key
-    }
-    if (secret_key != req.body.secret_key) {
-        changes["secret_key"] = req.body.secret_key
-    }
-    
-    if (changes != []) {
-        req.changes = changes
-        next()
-    } else {
-        next({status: 400, message: 'input valid change'})
-    }
-}
-
-module.exports = { validInput, existsConfig, compareChange }
+module.exports = { validInput, existsConfig }
