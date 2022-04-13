d79914a285bef26ba92e2a86db4e2a09c1406dc7
yuririnnnu <yrk96424@gmail.com>
2022-04-12 21:24:41 -0400

    changed number for the data

--- data/seeds/08-order_states.js
+++ data/seeds/08-order_states.js
@@ -6,9 +6,9 @@ exports.seed = function(knex) {
         {order_state_id: 2, description: 'SUBMITTED'},
         {order_state_id: 3, description: 'SUBMIT_REJECTED'},
         {order_state_id: 4, description: 'CANCEL'},
-        {order_state_id: 6, description: 'CANCELED'},
-        {order_state_id: 7, description: 'PARTIAL_EXEC'},
-        {order_state_id: 8, description: 'EXECUTED'},
+        {order_state_id: 5, description: 'CANCELED'},
+        {order_state_id: 6, description: 'PARTIAL_EXEC'},
+        {order_state_id: 7, description: 'EXECUTED'},
       ]);
     })
 };
